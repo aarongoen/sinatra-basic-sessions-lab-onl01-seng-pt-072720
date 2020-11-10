4d3f9e651a662f18edf384bd305248eff34085b5
aarongoen <aarongoen2.0@gmail.com>
2020-11-09 21:17:59 -0500

    Done.

--- app.rb
+++ app.rb
@@ -4,18 +4,24 @@ class App < Sinatra::Base
 
     configure do
         enable :sessions
-        set session_secret, "TU^Wm,Gsn/HgZY{L1ECxiK5gtsiqz8O~w'7Hig<>_'qKW,xWACpK`dIwN+g//(8"
+        set session_secret, "secret"
     end
 
     get '/' do
-        erb :'/index'
+        erb :index
     end
 
     post '/checkout' do
-        # binding.pry
-        @session = session.merge!(params)
-        @item = session["item"]
-        @items = Item.all
-        erb :'/checkout'
+       @items = "text test"
+        # c = a.merge(b)
+        
+        session.merge(params)
+        params["item"] = 
+        @session = session
+        # params["item"] = 
+        # @session = session.merge!(params)
+        @session["item"] = params[:item]
+        # # @items = Item.all"
+        erb :checkout
         end
 end
